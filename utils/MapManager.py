ef1ce879ce2d295efaa50d708750ef86e4bb03b3
JebobaTea <73501717+JebobaTea@users.noreply.github.com>
2026-04-28 22:42:07 -0400

    A* works

--- utils/MapManager.py
+++ utils/MapManager.py
@@ -1,6 +1,10 @@
 import numpy as np
 import time
+import random
 from heapq import heapify, heappop, heappush
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import inferno
+
 np.set_printoptions(precision=4)
 
 from utils.UtilsMisc import *
@@ -56,7 +60,6 @@ class World:
             np.save(f, np.array(wpts))
 
 
-
 class CellGrid:
     def __init__(self):
         self.cellmap = {}
@@ -77,22 +80,23 @@ class CellGrid:
         self.march(c, w)
         return time.process_time() - tstart
 
-    def getNeighbors(self, c):
-        return [self.getCell(c.x + 1, c.y + 1),
-                self.getCell(c.x + 1, c.y),
-                self.getCell(c.x + 1, c.y - 1),
-                self.getCell(c.x, c.y + 1),
-                self.getCell(c.x, c.y - 1),
-                self.getCell(c.x - 1, c.y + 1),
-                self.getCell(c.x - 1, c.y),
-                self.getCell(c.x - 1, c.y - 1)]
+    def getNeighbors(self, c, parent=None):
+        return [self.getCell(c.x + 1, c.y + 1, parent),
+                self.getCell(c.x + 1, c.y, parent),
+                self.getCell(c.x + 1, c.y - 1, parent),
+                self.getCell(c.x, c.y + 1, parent),
+                self.getCell(c.x, c.y - 1, parent),
+                self.getCell(c.x - 1, c.y + 1, parent),
+                self.getCell(c.x - 1, c.y, parent),
+                self.getCell(c.x - 1, c.y - 1, parent)]
 
     # returns empty by default, assumes LiDAR has not discovered it or the location is unoccupied
-    def getCell(self, x, y):
+    def getCell(self, x, y, parent=None):
         if x not in self.cellmap:
             self.cellmap[x] = {}
         if y not in self.cellmap[x]:
-            self.cellmap[x][y] = Cell(x, y, 0)
+            # default weight is 1
+            self.cellmap[x][y] = Cell(x, y, 1, parent)
         return self.cellmap[x][y]
 
     def march(self, c, start_weight):
@@ -104,51 +108,113 @@ class CellGrid:
                 self.march(cell, self.decay(start_weight))
 
     def decay(self, w):
-        return w/4
+        return w/2
 
-    def dijkstra(self, start_cell):
-        pq = [(0), start_cell]
+    def astar(self, start_cell, end_cell):
+        if math.isinf(end_cell.weight):
+            print("Destination occupied")
+            return
+        if math.isinf(start_cell.weight):
+            print("Start occupied")
+            return
+        # index 1 added because pq starts eating into subsequent indices of the tuple when comparison of 1st item is equal
+        pq = [(0, 0, start_cell)]
         heapify(pq)
         visited = set()
+        modified = set()
 
-        distances = {}
-        distances[start_cell] = 0
-
+        vis = False
+        if vis:
+            pt = np.array([[start_cell.x, start_cell.y], [end_cell.x, end_cell.y]])
+            plt.show()
+        framecount = 0
+        skip = 10
         while pq:
-            curr_dist, curr_cell = heappop(pq)
+            _, _, curr_cell = heappop(pq)
+            framecount += 1
+
+            # debug
+            if curr_cell.weight > 256:
+                print(f"Why are we navigating here: {curr_cell.x}, {curr_cell.y} | {curr_cell.weight} + {curr_cell.f} + {curr_cell.h}")
 
             if curr_cell in visited:
                 continue
+            else:
+                if curr_cell is start_cell:
+                    modified.add(curr_cell)
+                    curr_cell.f = 0
+                    curr_cell.g = 0
+                    curr_cell.h = 0
+                    curr_cell.parent = curr_cell
             visited.add(curr_cell)
+            # visualization for debugging
+            if vis and framecount % skip == 0:
+                pt = np.vstack((pt, [curr_cell.x, curr_cell.y]))
+                plt.clf()
+                data = np.load("world/pixels.npz")
+                cp = []
+                for i in data[:]:
+                    if i[2] == 0:
+                        continue
+                    i[2] = math.sqrt(i[2])
+                    i[2] /= 64
+                    cp.append([i[0], i[1], i[2]])
+                cp = np.array(cp)
+                plt.scatter(cp[:, 0], cp[:, 1], s=4, c=cp[:, 2], cmap="gray_r")
+                plt.scatter(pt[:, 0], pt[:, 1], s=4, c="red")
+                plt.pause(0.01)
+            # print("Visiting " + str(curr_cell.x) + ", " + str(curr_cell.y))
 
-            for neighbor in self.getNeighbors(curr_cell):
-                tentative_dist = curr_dist + neighbor.weight
-                if neighbor not in distances:
-                    distances[neighbor] = float("inf")
-                if tentative_dist < distances[neighbor]:
-                    distances[neighbor] = tentative_dist
-                    heappush(pq, (tentative_dist, neighbor))
+            for neighbor in self.getNeighbors(curr_cell, curr_cell):
+                if neighbor is end_cell:
+                    neighbor.parent = curr_cell
+                    print("Found path")
 
-        predecessors = {cell: None for cell in distances.keys()}
-        for cell, distance in distances.items():
-            for neighbor in self.getNeighbors(cell):
-                if distances[neighbor] == distance + neighbor.weight:
-                    predecessors[neighbor] = cell
-        return distances, predecessors
+                    # rebuild path
+                    path = []
+                    ccell = end_cell
+                    while ccell is not start_cell and ccell is not None:
+                        path.append([ccell.x, ccell.y])
+                        ccell = ccell.parent
+                    self.flush(modified)
+                    return path
+                gn = curr_cell.g + neighbor.weight
+                hn = math.sqrt(math.pow(neighbor.x - end_cell.x, 2) + math.pow(neighbor.y - end_cell.y, 2))
+                fn = gn + hn
+                if neighbor not in visited or neighbor.f > fn:
+                    neighbor.g = gn
+                    neighbor.h = hn
+                    neighbor.f = fn
+                    neighbor.parent = curr_cell
+                    heappush(pq, (fn, hn + random.random(), neighbor))
+                    modified.add(neighbor)
+        print("No path found")
+        flush(modified)
 
-    def shortest_path(self, start, end):
-        _, predecessors = self.dijkstra(start)
-        path = []
-        curr_cell = end
-        while curr_cell:
-            path.append(curr_cell)
-            curr_cell = predecessors[curr_cell]
-        path.reverse()
+    def flush(self, modded):
+        for cell in modded:
+            cell.f = float("inf")
+            cell.g = float("inf")
+            cell.h = 0
+            cell.parent = None
+
+    def generateWaypoints(self, x1, x2, y1, y2):
+        startCell = self.getCell(x1, x2)
+        endCell = self.getCell(y1, y2)
+        print("Beginning path search")
+        tstart = time.process_time()
+        path = self.astar(startCell, endCell)
+        print("End of search, took " + str(time.process_time() - tstart))
+        print(path)
         return path
 
 
 class Cell:
-    def __init__(self, x, y, weight):
+    def __init__(self, x, y, weight, parent=None):
         self.x = x
         self.y = y
         self.weight = weight
+        self.parent = parent
+        self.f = float("inf")
+        self.h = 0
+        self.g = float("inf")
